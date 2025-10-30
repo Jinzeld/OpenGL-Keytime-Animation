d5369c076b36f43c649e94f19bac5d0ad980d26e
Jinhui Zhen <123280886+Jinzeld@users.noreply.github.com>
2025-10-29 23:29:40 -0700

    Updated the file

--- loadobjmtlfiles.h
+++ loadobjmtlfiles.h
@@ -1,60 +1,64 @@
-#ifndef LOADOBJFILE_H
-#define LOADOBJFILE_H
+	#ifndef LOADOBJFILE_H
+	#define LOADOBJFILE_H
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <math.h>
-#include <ctype.h>
-#include <GL/gl.h>
-#include <vector>
+	#ifdef __APPLE__
+	#include <OpenGL/gl.h>
+	#else
+	#include <GL/gl.h>
+	#endif
 
-
-#define OBJDELIMS		" \t"
-
-const void  *	NULLPTR =     (void *)0;
-
-const char *	DELIMS = " \t";
-
-struct Vertex
-{
-	float x, y, z;
-};
-
-struct Normal
-{
-	float nx, ny, nz;
-};
-
-struct TextureCoord
-{
-	float s, t, p;
-};
-
-struct face
-{
-	int v, n, t;
-};
+	#include <stdio.h>
+	#include <string.h>
+	#include <stdlib.h>
+	#include <math.h>
+	#include <ctype.h>
+	#include <vector>
 
 
-GLuint	LoadObjFile( char * );
+	#define OBJDELIMS		" \t"
+	#define NULLPTR 0
+	#define DELIMS " \t"
 
-void	Cross( float [3], float [3], float [3] );
-char *	ReadRestOfLine( FILE * );
-void	ReadObjVTN( char *, int *, int *, int * );
-float	Unit( float [3] );
-float	Unit( float [3], float [3] );
+	struct Vertex
+	{
+		float x, y, z;
+	};
 
-int	Readline( FILE *, char * );
-float * Array3( float, float, float );
-float * Array3( float * );
+	struct Normal
+	{
+		float nx, ny, nz;
+	};
 
-int	FindMtlName( char * );
-void	SetOpenglMtlProperties( );
-char *	GetSuffix( char * );
+	struct TextureCoord
+	{
+		float s, t, p;
+	};
+
+	struct face
+	{
+		int v, n, t;
+	};
 
 
-unsigned char * BmpToTexture( char *, int *, int * );
+	GLuint	LoadObjFile( char * );
+	GLuint  LoadObjMtlFiles(char *name);
+
+	void	Cross( float [3], float [3], float [3] );
+	char *	ReadRestOfLine( FILE * );
+	void	ReadObjVTN( char *, int *, int *, int * );
+	float	Unit( float [3] );
+	float	Unit( float [3], float [3] );
+
+	int	Readline( FILE *, char * );
+	float * Array3( float, float, float );
+	float * Array3( float * );
+
+	int	FindMtlName( char * );
+	void	SetOpenglMtlProperties( );
+	char *	GetSuffix( char * );
 
 
-#endif		// #ifndef
+	unsigned char * BmpToTexture( char *, int *, int * );
+
+
+	#endif		// #ifndef
